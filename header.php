1d8ad579190688a74e95271cfe50d736c9816cef
MuhamadGhilbramZyaulhaq13 <muhamadghilbramzyaulhaq@gmail.com>
2025-11-24 11:28:37 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Php DataBase</title>
+   <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="container">
+    <h1>Data Barang</h1>
+    <div class="main">
+<nav>
+<a href="index.php" class="btn-tambah">Home</a>
+<a href="about.php" class="btn-tambah">Tentang</a>
+<a href="kontak.php" class="btn-tambah" >Kontak</a>
+</nav>
